f2255b8b8aad41d48f35aefb2913e45264b4915b
AterCattus <a3361da9>
2020-04-18 17:21:48 +0300

    code cleanup

--- scenes/scene_internals.lua
+++ scenes/scene_internals.lua
@@ -2,7 +2,7 @@ return function(scene)
     local composer = require("composer")
     local utils = require("libs.utils")
 
-    scene.mousePos = {x=-1, y=-1}
+    scene.mousePos = { x = -1, y = -1 }
     scene.updates = {} -- (scene, deltaTime)
 
     function scene:onEnterFrame(event)
@@ -18,8 +18,8 @@ return function(scene)
 
     local function onMouseEvent(event)
         --local W, H = display.contentWidth, display.contentHeight
-        scene.mousePos.x = event.x -- - W / 2
-        scene.mousePos.y = event.y -- - H / 2
+        scene.mousePos.x = event.x
+        scene.mousePos.y = event.y
         --self.pressedKeys.mouseLeft = event.isPrimaryButtonDown
     end
 
@@ -41,31 +41,13 @@ return function(scene)
     end)
 
     function scene:addUpdate(cb)
-        scene.updates[#scene.updates+1] = cb
+        scene.updates[#scene.updates + 1] = cb
     end
 
     function scene:update(deltaTime)
         for i = 1, #scene.updates do
             scene.updates[i](scene, deltaTime)
         end
-        --for i = 1, #self.reqInFlight do
-        --    local req = self.reqInFlight[i]
-        --
-        --    req.y = req.y + req.speed * deltaTime
-        --    if req.y > H - const.BottomPanelHeight then
-        --        req.y = const.TopPanelHeight - ReqHeight * ReqRenderScale / 2
-        --    end
-        --end
-        --
-        --for i = 1, #self.updates do
-        --    self.updates[i](scene, deltaTime)
-        --end
-        --
-        --local x = scene.mousePos.x
-        --if x < 0 then
-        --    x = W/2
-        --end
-        --scene.objs.player.x = x
     end
 
 
